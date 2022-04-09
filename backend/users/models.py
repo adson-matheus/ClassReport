be3bcb61cb7aa11bc50d9531bf4d3013c6e48478
Adson <adson.matheus62@gmail.com>
2022-04-09 11:20:28 -0300

    create model administrador

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class administrador(models.Model):
+    siape = models.CharField(verbose_name='siape', primary_key=True, max_length=20, null=False)
+    nome = models.CharField(verbose_name='nome', max_length=100, null=False)
+    usuario = models.CharField(verbose_name='usuario', max_length=20, null=False)
+    senha = models.CharField(verbose_name='senha', max_length=20, null=False)
