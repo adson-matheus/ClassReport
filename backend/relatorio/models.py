43366335a1b7aa82395187e95f89aacf8f10803f
adson62 <adson.matheus62@gmail.com>
2022-04-11 09:50:34 -0300

    model Avaliacao e Relatorio

--- backend/relatorio/models.py
+++ backend/relatorio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import Professor, Administrador
+from aula.models import Area, Aula
 
 # Create your models here.
+class Avaliacao(models.Model):
+    idAula = models.ForeignKey(Aula, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
+    idAluno = models.ForeignKey(Professor, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
+
+class Relatorio(models.Model):
+    idAdmin = models.ForeignKey(Administrador, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
+    idAvaliacao = models.ForeignKey(Avaliacao, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
+    idArea = models.ForeignKey(Area, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
