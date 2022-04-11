104efce804e0a4ff02eb1d6264a9a4b64b4daff4
adson62 <adson.matheus62@gmail.com>
2022-04-11 11:17:19 -0300

    Model Avaliacao e Relatorio do Administrador

--- backend/relatorio/models.py
+++ backend/relatorio/models.py
@@ -1,13 +1,28 @@
 from django.db import models
-from users.models import Professor, Administrador
-from aula.models import Area, Aula
+from users.models import Aluno, Administrador
+from aula.models import Area, AulaDoAluno
 
 # Create your models here.
 class Avaliacao(models.Model):
-    idAula = models.ForeignKey(Aula, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
-    idAluno = models.ForeignKey(Professor, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
+    aula_do_aluno = models.ForeignKey(AulaDoAluno, on_delete=models.CASCADE, null=False)
+    checklist = models.JSONField(verbose_name='checklist')
+
+    class Meta:
+        ordering = ('-id',)
+    def __str__(self):
+        return 'Avaliação da aula de {}'.format(self.aula_do_aluno)
 
 class Relatorio(models.Model):
-    idAdmin = models.ForeignKey(Administrador, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
-    idAvaliacao = models.ForeignKey(Avaliacao, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
-    idArea = models.ForeignKey(Area, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
+    idAdmin = models.ForeignKey(Administrador, verbose_name='Administrador', on_delete=models.CASCADE, null=False)
+    idAvaliacao = models.ForeignKey(Avaliacao, verbose_name='Avaliacao', on_delete=models.CASCADE, null=False)
+    idArea = models.ForeignKey(Area, verbose_name='Area', on_delete=models.CASCADE, null=False)
+    relatorio = models.TextField(verbose_name='Relatório', max_length=2000, null=False)
+
+    class Meta:
+        ordering = ('-id',)
+    def __str__(self):
+        return 'Relatório de {} - {}'.format(self.idAdmin, self.idAvaliacao)
+
+class AvaliacoesDoRelatorio(models.Model):
+    idAvaliacao = models.ForeignKey(Avaliacao, on_delete=models.CASCADE, null=False)
+    idRelatorio = models.ForeignKey(Relatorio, on_delete=models.CASCADE, null=False)
