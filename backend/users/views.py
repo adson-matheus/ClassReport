950ced12cb75935584f36f2114d74e06a637c54b
Adson <adson.matheus62@gmail.com>
2022-04-11 20:36:51 -0300

    template para add admin

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Administrador
+from django.contrib.auth.models import UserManager
 
 # Create your views here.
 def index(request):
     return HttpResponse("hello, world")
+
+def add_admin_template(request):
+    return render(request, 'users/add_admin_template.html')
+
