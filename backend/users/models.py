b79d32bc8845871685324ea202fb1982b17c57ba
adson62 <adson.matheus62@gmail.com>
2022-04-11 09:50:18 -0300

    models Professor

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from aula.models import Area
 
 # Create your models here.
 class Administrador(models.Model):
@@ -11,7 +12,7 @@ class Aluno(models.Model):
     matricula = models.CharField(verbose_name='matricula', primary_key=True, max_length=20, null=False)
     nome = models.CharField(verbose_name='nome', primary_key=False, max_length=100, null=False)
 
-# class Professor(models.Model):
-#     siape = models.CharField(verbose_name='siape', primary_key=True, max_length=20, null=False)
-#     nome = models.CharField(verbose_name='nome', primary_key=False, max_length=100, null=False)
-#     idArea = models.ForeignKey(Area, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
+class Professor(models.Model):
+    siape = models.CharField(verbose_name='siape', primary_key=True, max_length=20, null=False)
+    nome = models.CharField(verbose_name='nome', primary_key=False, max_length=100, null=False)
+    idArea = models.ForeignKey(Area, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
