3e14997c1fa370adf117ca756fe8c7a77fe2f085
adson62 <adson.matheus62@gmail.com>
2022-04-11 11:16:36 -0300

    usar a parte admin para registrar os models

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Administrador)
+admin.site.register(Professor)
+admin.site.register(Aluno)
+admin.site.register(Area)
