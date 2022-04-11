e2ddc3e069174fe366c48c8848071aa816462010
adson62 <adson.matheus62@gmail.com>
2022-04-11 11:17:40 -0300

    Model Administrador, Aluno, Professor e Area

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,18 +1,42 @@
 from django.db import models
-from aula.models import Area
 
 # Create your models here.
+class Area(models.Model):
+    nome = models.CharField(verbose_name='nome', primary_key=False, max_length=100, null=False)
+    
+    class Meta:
+        ordering = ('-id',)
+    def __str__(self):
+        return self.nome
+
 class Administrador(models.Model):
     siape = models.CharField(verbose_name='siape', primary_key=True, max_length=20, null=False)
     nome = models.CharField(verbose_name='nome', max_length=100, null=False)
     usuario = models.CharField(verbose_name='usuario', max_length=20, null=False)
     senha = models.CharField(verbose_name='senha', max_length=20, null=False)
 
+    class Meta:
+        ordering = ('-nome',)
+    def __str__(self):
+        return self.nome
+
 class Aluno(models.Model):
     matricula = models.CharField(verbose_name='matricula', primary_key=True, max_length=20, null=False)
     nome = models.CharField(verbose_name='nome', primary_key=False, max_length=100, null=False)
 
+    class Meta:
+        ordering = ('-nome',)
+    def __str__(self):
+        return self.nome
+
 class Professor(models.Model):
     siape = models.CharField(verbose_name='siape', primary_key=True, max_length=20, null=False)
-    nome = models.CharField(verbose_name='nome', primary_key=False, max_length=100, null=False)
-    idArea = models.ForeignKey(Area, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
+    nome = models.CharField(verbose_name='nome', max_length=100, null=False)
+    usuario = models.CharField(verbose_name='usuario', max_length=20, null=False)
+    senha = models.CharField(verbose_name='senha', max_length=20, null=False)
+    idArea = models.ForeignKey(Area, on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        ordering = ('-nome',)
+    def __str__(self):
+        return self.nome
