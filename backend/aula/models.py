c22f09cd020cd90d2ab9f91c45a019b1355b3345
adson62 <adson.matheus62@gmail.com>
2022-04-11 11:17:05 -0300

    model Aula e AulaDoAluno

--- backend/aula/models.py
+++ backend/aula/models.py
@@ -1,14 +1,24 @@
 from django.db import models
-from users.models import Aluno
+from users.models import Professor, Aluno, Area
 
 # Create your models here.
-class Area(models.Model):
-    nome = models.CharField(verbose_name='nome', primary_key=False, max_length=100, null=False)
-
 class Aula(models.Model):
-    idArea = models.ForeignKey(Area, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
+    idArea = models.ForeignKey(Area, on_delete=models.CASCADE, null=False)
+    idProfessor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    assunto = models.CharField(verbose_name='Assunto da aula', max_length=50, null=False)
     datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
 
+    class Meta:
+        ordering = ('-id',)
+    def __str__(self):
+        return '[{}] Aula: {} - Prof. {}, Área {}'.format(self.datetime, self.assunto, self.idProfessor, self.idArea)
+
+
 class AulaDoAluno(models.Model):
-    idAula = models.ForeignKey(Aula, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
-    idAluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, on_update=models.CASCADE, null=False)
+    idAula = models.ForeignKey(Aula, on_delete=models.CASCADE, null=False)
+    idAluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        ordering = ('-id',)
+    def __str__(self):
+        return '{} - {}'.format(self.idAluno, self.idAula)
