0a02e4f3a367c6cad43e8e7fe93da71c1b46f2ea
adson62 <adson.matheus62@gmail.com>
2022-04-12 10:26:53 -0300

    add view para add admin

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from users import views
 from django.urls import path
 
+app_name = 'users'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/admin/', views.add_admin_template, name='add_admin_template'),
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Administrador
+from django.shortcuts import redirect, render
+from .forms import AddAdministradorForm
 from django.contrib.auth.models import UserManager
 
 # Create your views here.
@@ -8,5 +8,11 @@ def index(request):
     return HttpResponse("hello, world")
 
 def add_admin_template(request):
-    return render(request, 'users/add_admin_template.html')
-
+    if request.method == 'POST':
+        form = AddAdministradorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users:index')
+    else:
+        form = AddAdministradorForm()
+    return render(request, 'users/add_admin_template.html', {'form': form})
