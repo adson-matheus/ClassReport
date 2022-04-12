e34fc78b6f95d78377a523038974b3c94ab31755
adson62 <adson.matheus62@gmail.com>
2022-04-12 10:55:52 -0300

    mostrar areas cadastradas ao cadastrar prof

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from .models import Area
 from .forms import AddAdministradorForm, AddProfessorForm
 from django.contrib.auth.models import UserManager
 
@@ -18,6 +19,7 @@ def add_admin_template(request):
     return render(request, 'users/add_admin_template.html', {'form': form})
 
 def add_prof_template(request):
+    areas = Area.objects.all()
     if request.method == 'POST':
         form = AddProfessorForm(request.POST)
         if form.is_valid():
@@ -25,4 +27,4 @@ def add_prof_template(request):
             return redirect('users:index')
     else:
         form = AddProfessorForm()
-    return render(request, 'users/add_prof_template.html', {'form': form})
+    return render(request, 'users/add_prof_template.html', {'form': form, 'areas': areas})
