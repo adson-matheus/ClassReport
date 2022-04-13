3330dac84a683fbb1a757dcbe849f709f36f4007
adson62 <adson.matheus62@gmail.com>
2022-04-13 09:46:46 -0300

    update settings.py

--- backend/ClassReport/settings.py
+++ backend/ClassReport/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    #BASE_DIR / "static",
     '../frontend/static/',
 ]
 
