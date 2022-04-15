4373098c10395be54f4f17e0a8fb7d1f2b5bb67b
Adson <adson.matheus62@gmail.com>
2022-04-15 19:59:45 -0300

    usando relacionamento 1:1 ao inves de herança

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Area(models.Model):
     nome = models.CharField(verbose_name='nome', primary_key=False, max_length=100, null=False)
     
@@ -9,20 +8,6 @@ class Area(models.Model):
         ordering = ('-id',)
     def __str__(self):
         return self.nome
-
-class Administrador(User):
-    siape = models.CharField(verbose_name='siape', primary_key=True, max_length=20, null=False)
-
-    # def add_administrador(siape, nome, email, password):
-    #     user = User.objects.create_user(nome, email, password)
-    #     user.siape = siape
-    #     user.save()
-
-
-    class Meta:
-        ordering = ('first_name',)
-    def __str__(self):
-        return User.get_full_name(self)
 
 class Aluno(models.Model):
     matricula = models.CharField(verbose_name='matricula', primary_key=True, max_length=20, null=False)
@@ -33,11 +18,21 @@ class Aluno(models.Model):
     def __str__(self):
         return self.nome
 
-class Professor(User):
+class Administrador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    siape = models.CharField(verbose_name='siape', primary_key=True, max_length=20, null=False)
+
+    class Meta:
+        ordering = ('-user',)
+    def __str__(self):
+        return self.user.username
+
+class Professor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     siape = models.CharField(verbose_name='siape', primary_key=True, max_length=20, null=False)
     idArea = models.ForeignKey(Area, on_delete=models.CASCADE, null=False)
 
     class Meta:
-        ordering = ('first_name',)
+        ordering = ('-user',)
     def __str__(self):
-        return User.get_full_name(self)
+        return self.user.username
