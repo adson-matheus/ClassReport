e9a7656226ff791a86e447dfcb5555d623536ba5
Adson <adson.matheus62@gmail.com>
2022-04-23 12:59:54 -0300

    add urls para a API com as areas #9

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 app_name = 'users'
 urlpatterns = [
-    path('add/admin/', views.add_admin_template, name='add_admin_template'),
-    path('add/prof/', views.add_prof_template, name='add_prof_template'),
+    path('prof/add/', views.add_prof_template, name='add_prof_template'),
+    path('admin/add/', views.add_admin_template, name='add_admin_template'),
+    path('admin/areas/', views.ListarAreas.as_view()),
+    path('admin/areas/<int:pk>', views.DetalhesAreas.as_view()),
 ]
