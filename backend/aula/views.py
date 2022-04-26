73918ce885e4b58d47c0f7e7cf1c0b04ea67b40e
adson62 <adson.matheus62@gmail.com>
2022-04-26 11:07:19 -0300

    view crud aula #15

--- backend/aula/views.py
+++ backend/aula/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Aula
 
-# Create your views here.
+def crud_aula(request, username):
+    context = {
+        #'queryset': Aula.objects.get('idProfessor = {}'.format(idProfessor)),
+        'username': request.user.username,
+        'full_name': request.user.get_full_name()
+    }
+    return render(request, 'aula/aulas.html', context)
