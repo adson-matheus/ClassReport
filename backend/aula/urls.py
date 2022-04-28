3626d5f82419af0f1c0860ce789624cef7c4c3f9
adson62 <adson.matheus62@gmail.com>
2022-04-28 11:07:03 -0300

    url para add aula #15

--- backend/aula/urls.py
+++ backend/aula/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 app_name = 'aulas'
 urlpatterns = [
-    path('prof/<username>', views.crud_aula, name='aulas')
+    path('prof/<username>', views.AulaTemplate.index, name='aulas'),
+    path("prof/<username>/add", views.AulaTemplate.add_aula, name='add_aula')
 ]
