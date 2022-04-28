9ce516a797bfe8def967947f7b9d4abd7ebb5e4e
adson62 <adson.matheus62@gmail.com>
2022-04-28 11:07:34 -0300

    view add aula; criada classe AulaTemplate p/ o CRUD #15

--- backend/aula/views.py
+++ backend/aula/views.py
@@ -1,10 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from users.models import Professor, Area
+from .forms import AulaForm
 from .models import Aula
+from datetime import datetime
+class AulaTemplate:
+    def index(request, username):
+        context = {
+            #'queryset': Aula.objects.get('idProfessor = {}'.format(idProfessor)),
+            'username': username,
+            'full_name': request.user.get_full_name()
+        }
+        return render(request, 'aula/aulas.html', context)
+    def add_aula(request, username):
+        if request.method == 'POST':
+            form_aula = AulaForm(request.POST)
+            print(form_aula)
+            if form_aula.is_valid():
+                idArea = form_aula.cleaned_data['idArea']
+                assunto = form_aula.cleaned_data['assunto']
+                dataAula = form_aula.cleaned_data['dataAula']
+                horario = form_aula.cleaned_data['horario']
 
-def crud_aula(request, username):
-    context = {
-        #'queryset': Aula.objects.get('idProfessor = {}'.format(idProfessor)),
-        'username': request.user.username,
-        'full_name': request.user.get_full_name()
-    }
-    return render(request, 'aula/aulas.html', context)
+                dt = datetime(year=dataAula.year,
+                              month=dataAula.month,
+                              day=dataAula.day,
+                              hour=horario.hour,
+                              minute=horario.minute)
+                print(dt)
+
+                Aula(idProfessor=Professor.objects.get(user=request.user), idArea=idArea, assunto=assunto, datetime=dt).save()
+                return redirect('aulas:aulas', request.user.username)
+            else:
+                pass
+                #message
+        else:
+            form_aula = AulaForm()
+        context = {
+            'form_aula':form_aula,
+            'full_name': request.user.get_full_name(),
+            'areas': Area.objects.all()
+        }
+        return render(request, 'aula/add_aula.html', context)
