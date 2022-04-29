56f85eefe12c7b3d90727f7c9e9e28cc523cc1f2
adson62 <adson.matheus62@gmail.com>
2022-04-29 16:08:24 -0300

    queryset com aulas do prof #15

--- backend/aula/views.py
+++ backend/aula/views.py
@@ -3,10 +3,12 @@ from users.models import Professor, Area
 from .forms import AulaForm
 from .models import Aula
 from datetime import datetime
+
 class AulaTemplate:
     def index(request, username):
+        prof = Professor.objects.get(user=request.user)
         context = {
-            #'queryset': Aula.objects.get('idProfessor = {}'.format(idProfessor)),
+            'queryset': Aula.objects.filter(idProfessor = prof),
             'username': username,
             'full_name': request.user.get_full_name()
         }
@@ -14,19 +16,16 @@ class AulaTemplate:
     def add_aula(request, username):
         if request.method == 'POST':
             form_aula = AulaForm(request.POST)
-            print(form_aula)
             if form_aula.is_valid():
                 idArea = form_aula.cleaned_data['idArea']
                 assunto = form_aula.cleaned_data['assunto']
                 dataAula = form_aula.cleaned_data['dataAula']
                 horario = form_aula.cleaned_data['horario']
-
                 dt = datetime(year=dataAula.year,
                               month=dataAula.month,
                               day=dataAula.day,
                               hour=horario.hour,
                               minute=horario.minute)
-                print(dt)
 
                 Aula(idProfessor=Professor.objects.get(user=request.user), idArea=idArea, assunto=assunto, datetime=dt).save()
                 return redirect('aulas:aulas', request.user.username)
