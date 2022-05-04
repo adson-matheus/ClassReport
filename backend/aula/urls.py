8cae2926aac58b1e830f87c7c8a9fbf077338043
adson62 <adson.matheus62@gmail.com>
2022-05-04 07:58:37 -0300

    url para detalhar aula #15

--- backend/aula/urls.py
+++ backend/aula/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 app_name = 'aulas'
 urlpatterns = [
     path('prof/<username>', views.AulaTemplate.index, name='aulas'),
-    path("prof/<username>/add", views.AulaTemplate.add_aula, name='add_aula')
+    path("prof/<username>/add", views.AulaTemplate.add_aula, name='add_aula'),
+    path("prof/<username>/detail/<int:id>", views.AulaTemplate.get_aula, name='get_aula')
 ]
