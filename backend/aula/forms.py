5be778dbe1a9a9da071e66741dac8e432787bb81
adson62 <adson.matheus62@gmail.com>
2022-05-05 10:26:46 -0300

    edit aula form #15

--- backend/aula/forms.py
+++ backend/aula/forms.py
@@ -1,5 +1,5 @@
 from django.utils.translation import gettext_lazy as _
-from django.forms import ModelForm, Textarea, TimeField, DateField
+from django.forms import ModelForm, TimeField, DateField
 from .models import Aula
 
 class AulaForm(ModelForm):
@@ -8,17 +8,10 @@ class AulaForm(ModelForm):
     class Meta:
         model = Aula
         exclude = ['idProfessor', 'datetime']
-        # widgets = {
-        #     'assunto': Textarea(attrs={'cols': 50, 'rows': 15}),
-        # }
-        # help_texts = {
-        #     'assunto': _('O assunto/descrição da aula'),
-        # }
-        # error_messages = {
-        #     'name': {
-        #         'max_length': _("This writer's name is too long."),
-        #     },
-        # }
-        # labels = {
-        #     'assunto': _(''),
-        # }
+
+class AulaFormEdit(ModelForm):
+    dataAula = DateField(required=True, label='Data da aula', initial=Aula.datetime)
+    horario = TimeField(required=True, label='Horário da aula')
+    class Meta:
+        model = Aula
+        exclude = ['idProfessor',]
