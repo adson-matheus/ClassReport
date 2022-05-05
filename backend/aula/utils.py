95e925be546b7f01b9dbd5833dff705a6aa265b0
adson62 <adson.matheus62@gmail.com>
2022-05-05 11:02:39 -0300

    funcao pra retornar hora e data #15

--- backend/aula/utils.py
+++ backend/aula/utils.py
@@ -1,11 +1,12 @@
-from datetime import datetime
+from datetime import date, datetime
 
 def converter_para_datetime(data, horario):
     """
         Recebe um date e time e retorna uma datetime.
     """
-    return datetime(year=data.year,
-                    month=data.month,
-                    day=data.day,
-                    hour=horario.hour,
-                    minute=horario.minute)
+    return datetime.combine(date=data, time=horario)
+
+def converter_de_datetime(date_time):
+    data = datetime.date(date_time)
+    hora = datetime.time(date_time)
+    return data, hora
