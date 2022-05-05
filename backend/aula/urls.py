45a6d20dc5e7eb669ebed242d7619a0ba8910363
adson62 <adson.matheus62@gmail.com>
2022-05-05 09:54:40 -0300

    url para editar uma aula #15

--- backend/aula/urls.py
+++ backend/aula/urls.py
@@ -1,9 +1,10 @@
 from aula import views
 from django.urls import path
 
-app_name = 'aulas'
+app_name = 'aula'
 urlpatterns = [
-    path('prof/<username>', views.AulaTemplate.index, name='aulas'),
+    path("prof/<username>", views.AulaTemplate.index_aula, name='index_aula'),
     path("prof/<username>/add", views.AulaTemplate.add_aula, name='add_aula'),
-    path("prof/<username>/detail/<int:id>", views.AulaTemplate.get_aula, name='get_aula')
+    path("prof/<username>/detail/<int:id>", views.AulaTemplate.get_aula, name='get_aula'),
+    path("prof/<username>/edit/<int:id>", views.AulaTemplate.edit_aula, name='edit_aula'),
 ]
