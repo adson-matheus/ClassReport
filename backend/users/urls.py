a7985a9d11e24e2d0a86581e3dfc4af7bf344ea4
adson62 <adson.matheus62@gmail.com>
2022-05-10 09:25:24 -0300

    url p/ editar aluno #27

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 
 app_name = 'users'
 urlpatterns = [
-    path('prof/add/', views.add_prof_template, name='add_prof_template'),
-    path('admin/add/', views.add_admin_template, name='add_admin_template'),
-    path('admin/areas/', views.ListarAreas.as_view()),
+    path('prof/add', views.add_prof_template, name='add_prof_template'),
+    path('admin/add', views.add_admin_template, name='add_admin_template'),
+    path('admin/areas', views.ListarAreas.as_view()),
     path('admin/areas/<int:pk>', views.DetalhesAreas.as_view()),
-    path('aluno/add/', views.AlunoTemplate.add_aluno, name='add_aluno_template'),
+    path('aluno/add', views.AlunoTemplate.add_aluno, name='add_aluno'),
+    path('aluno/edit/<int:id>', views.AlunoTemplate.edit_aluno, name='edit_aluno'),
 ]
