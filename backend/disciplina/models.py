b9fffdf7df09a6a07553c4af65badd13b2736ac3
adson62 <adson.matheus62@gmail.com>
2022-05-18 10:48:01 -0300

    feat: mostrar nome e prof. da disciplina #47

--- backend/disciplina/models.py
+++ backend/disciplina/models.py
@@ -5,3 +5,8 @@ from users.models import Professor
 class Disciplina(models.Model):
     nome = models.CharField(verbose_name="Nome", max_length=255, null=True, blank=True)
     idProfessor = models.ForeignKey(Professor, verbose_name="Professor", on_delete=models.SET_NULL, blank=True, null=True)
+
+    class Meta:
+        ordering = ('-nome',)
+    def __str__(self):
+        return 'Nome: {} - Prof. {}'.format(self.nome, self.idProfessor.user.get_full_name())
