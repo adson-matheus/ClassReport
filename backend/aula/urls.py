024b3250295492bf0fc85bb4f9801862ea01aed2
adson62 <adson.matheus62@gmail.com>
2022-05-19 07:28:09 -0300

    fix: rota p/ deletar aula

--- backend/aula/urls.py
+++ backend/aula/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("add", views.AulaTemplate.add_aula, name='add_aula'),
     path("detail/<int:id>", views.AulaTemplate.get_aula, name='get_aula'),
     path("prof/<username>/edit/<int:id>", views.AulaTemplate.edit_aula, name='edit_aula'),
-    path("prof/<username>/delete/<int:id>", views.AulaTemplate.delete_aula_template, name='delete_aula_template'),
-    path("prof/<username>/delete/<int:id>/confirm", views.AulaTemplate.delete_aula, name='delete_aula'),
+    path("delete/<int:id>", views.AulaTemplate.delete_aula_template, name='delete_aula_template'),
+    path("delete/<int:id>/confirm", views.AulaTemplate.delete_aula, name='delete_aula'),
 ]
