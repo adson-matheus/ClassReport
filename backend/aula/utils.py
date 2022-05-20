7eeb49a9d42d5199e9836970d8645462122592b0
adson62 <adson.matheus62@gmail.com>
2022-05-20 13:20:15 -0300

    feat: removendo funcoes nao utilizadas #63

--- backend/aula/utils.py
+++ backend/aula/utils.py
@@ -1,12 +1,1 @@
-from datetime import date, datetime
 
-def converter_para_datetime(data, horario):
-    """
-        Recebe um date e time e retorna uma datetime.
-    """
-    return datetime.combine(date=data, time=horario)
-
-def converter_de_datetime(date_time):
-    data = datetime.date(date_time)
-    hora = datetime.time(date_time)
-    return data, hora
