ba896076e9bc3fbb3390f3c0ab64d2b70f461330
adson62 <adson.matheus62@gmail.com>
2022-05-25 09:06:18 -0300

    feat: url p/ listar professor #51

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 
 app_name = 'users'
 urlpatterns = [
-    path('prof/add', views.add_prof_template, name='add_prof_template'),
     path('admin/add', views.add_admin_template, name='add_admin_template'),
     path('alunos', views.AlunoTemplate.index_aluno, name='index_aluno'),
     path('aluno/add', views.AlunoTemplate.add_aluno, name='add_aluno'),
     path('aluno/edit/<int:matr>', views.AlunoTemplate.edit_aluno, name='edit_aluno'),
     path('aluno/delete/<int:matr>', views.AlunoTemplate.delete_aluno_template, name='delete_aluno_template'),
     path('aluno/delete/<int:matr>/confirm', views.AlunoTemplate.delete_aluno, name='delete_aluno'),
+    path('professores', views.ProfessorTemplate.index_professor, name='index_prof'),
+    path('prof/add', views.add_prof_template, name='add_prof_template'),
 ]
