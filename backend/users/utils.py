052dbe9735697d2e93f9acdb0cfe151a2d70b1c4
adson62 <adson.matheus62@gmail.com>
2022-05-30 08:19:51 -0300

    feat: listar alunos em funcao separada da view #23

--- backend/users/utils.py
+++ backend/users/utils.py
@@ -1,7 +1,17 @@
 from django.contrib.auth.models import Group
+from .models import Aluno
 
 def is_admin(request):
     group = Group.objects.get(name='grp_administradores')
     if group in request.user.groups.all():
         return {'is_admin':True}
     else: return {'is_admin':False}
+
+def listar_alunos(request):
+    alunos = Aluno.objects.all()
+    context = {
+        'alunos': alunos,
+        'full_name': request.user.get_full_name()
+    }
+    context.update(is_admin(request))
+    return context
