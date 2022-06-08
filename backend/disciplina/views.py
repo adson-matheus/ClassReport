50db215f63e9a0d03db0e56610aaa027fd68b090
Adson Matheus <adson.matheus62@gmail.com>
2022-06-08 10:31:02 -0300

    feat: view p/ add disciplina #84

--- backend/disciplina/views.py
+++ backend/disciplina/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from disciplina.models import Disciplina
+from disciplina.forms import DisciplinaForm
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 from users.utils import is_admin
 
@@ -11,3 +12,22 @@ def listar_disciplinas(request):
     }
     context.update(is_admin(request))
     return render(request, 'disciplina/disciplina.html', context)
+
+@permission_required('disciplina.add_disciplina', login_url='/', raise_exception=True)
+def add_disciplina(request):
+    if request.method == 'POST':
+        form = DisciplinaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('disciplina:listar_disciplinas')
+        else:
+            pass
+    else:
+        form = DisciplinaForm()
+
+    context = {
+        'form': form,
+        'full_name': request.user.get_full_name(),
+        'is_admin': True,
+    }
+    return render(request, 'disciplina/add_disciplina.html', context)
