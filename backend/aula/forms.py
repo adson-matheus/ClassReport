698a998acb40cb9b4bf7e42363ab2c3eb8f16004
Adson Matheus <adson.matheus62@gmail.com>
2022-06-10 16:57:07 -0300

    feat: criar form p/ pegar os ids dos alunos marcados #23

--- backend/aula/forms.py
+++ backend/aula/forms.py
@@ -1,5 +1,5 @@
-from django.forms import ModelForm, TimeField, DateField
-from .models import Aula
+from django.forms import ModelForm
+from .models import Aula, AulaDoAluno
 
 class AulaForm(ModelForm):
     class Meta:
@@ -10,3 +10,8 @@ class AulaFormEdit(ModelForm):
     class Meta:
         model = Aula
         fields = '__all__'
+
+# criar um form p/ pegar todos os ids marcados
+# depois envia isso pra view
+# da view faz um bulk create,
+# usar js pra pegar todos os valores marcados
