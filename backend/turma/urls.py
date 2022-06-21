f317ab74c8e66d0a26ff21c76c046d769cc93015
Adson Matheus <adson.matheus62@gmail.com>
2022-06-21 11:09:40 -0300

    feat: url p/ excluir turma #86

--- backend/turma/urls.py
+++ backend/turma/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 app_name = 'turma'
 urlpatterns = [
     path('list', views.listar_turmas, name='listar_turmas'),
+    path('excluir/<int:id>', views.excluir_turma_template, name='excluir_turma_template'),
+    path('excluir/<int:id>/confirmar', views.excluir_turma, name='excluir_turma'),
 ]
