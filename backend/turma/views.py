a7faf66e947705862020eec80ed1fed0fe9ff875
Adson Matheus <adson.matheus62@gmail.com>
2022-06-21 10:33:35 -0300

    feat: view p/ listar turma #87

--- backend/turma/views.py
+++ backend/turma/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from users.utils import is_admin
+from .models import Turma
 
 # Create your views here.
+def listar_turmas(request):
+    turmas = Turma.objects.all()
+    context = {
+        'turmas': turmas,
+        'full_name': request.user.get_full_name(),
+    }
+    context.update(is_admin(request))
+    return render(request, 'turma/listar_turmas.html', context)
