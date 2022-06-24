0918901bcdafc6bf1aab6a0b74f56c89cbfb82bd
Adson Matheus <adson.matheus62@gmail.com>
2022-06-24 14:13:35 -0300

    feat: url p/ listar aulas de uma turma #113

--- backend/turma/urls.py
+++ backend/turma/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 app_name = 'turma'
 urlpatterns = [
     path('list', views.listar_turmas, name='listar_turmas'),
+    path('list/<int:id>', views.listar_aulas_de_turma, name='listar_aulas_de_turma'),
     path('excluir/<int:id>', views.excluir_turma_template, name='excluir_turma_template'),
     path('excluir/<int:id>/confirmar', views.excluir_turma, name='excluir_turma'),
 ]
