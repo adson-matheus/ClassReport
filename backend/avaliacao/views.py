439246736ad736494c5a3cd30a51d7a9dd48e54e
Adson Matheus <adson.matheus62@gmail.com>
2022-06-28 10:25:52 -0300

    feat: view p/ excluir uma avaliacao #125

--- backend/avaliacao/views.py
+++ backend/avaliacao/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, redirect, get_object_or_404
 from users.utils import is_admin
 from .utils import json_para_string
+from django.contrib import messages
 from aula.models import AulaDoAluno
 from .models import Avaliacao
 
@@ -16,3 +18,20 @@ def detalhar_avaliacao(request, id_aluno, id_aula):
     }
     context.update(is_admin(request))
     return render(request, 'avaliacao/detalhar_avaliacao.html', context)
+
+@permission_required('avaliacao.delete_avaliacao', login_url='/', raise_exception=True)
+def excluir_avaliacao_template(request, id_avaliacao):
+    avaliacao = get_object_or_404(Avaliacao, pk=id_avaliacao)
+    context = {
+        'full_name': request.user.get_full_name(),
+        'avaliacao': avaliacao,
+    }
+    context.update(is_admin(request))
+    return render(request, 'avaliacao/excluir_avaliacao_template.html', context)
+
+@permission_required('avaliacao.delete_avaliacao', login_url='/', raise_exception=True)
+def excluir_avaliacao(request, id_avaliacao):
+    avaliacao = get_object_or_404(Avaliacao, pk=id_avaliacao)
+    avaliacao.delete()
+    messages.success(request, f'Avaliação de {avaliacao.aula_do_aluno.aluno} excluída com sucesso!')
+    return redirect('aula:get_aula', avaliacao.aula_do_aluno.aula.id)
