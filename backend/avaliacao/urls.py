6f9f82209bf93c744c80adb8b2273d06ce197452
Adson Matheus <adson.matheus62@gmail.com>
2022-06-30 10:01:33 -0300

    feat: url p/ exportar pdf #128

--- backend/avaliacao/urls.py
+++ backend/avaliacao/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("<int:id_aula>/aluno/<int:id_aluno>", views.detalhar_avaliacao, name='detalhar_avaliacao'),
     path("<int:id_avaliacao>/excluir", views.excluir_avaliacao_template, name='excluir_avaliacao_template'),
     path("<int:id_avaliacao>/excluir/confirmar", views.excluir_avaliacao, name='excluir_avaliacao'),
+    path("<int:id_avaliacao>/pdf", views.exportar_pdf, name='exportar_pdf'),
 ]
