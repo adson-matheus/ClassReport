ebf7112bf2021b53556db266ed7585b8032e42ba
Adson Matheus <adson.matheus62@gmail.com>
2022-07-05 10:52:42 -0300

    feat: url p/ add avaliacao #127

--- backend/avaliacao/urls.py
+++ backend/avaliacao/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 app_name = 'avaliacao'
 urlpatterns = [
     # avaliacao
+    path("<int:id_aula>/aluno/<int:id_aluno>/add", views.adicionar_avaliacao, name='adicionar_avaliacao'),
     path("<int:id_aula>/aluno/<int:id_aluno>", views.detalhar_avaliacao, name='detalhar_avaliacao'),
     path("<int:id_avaliacao>/excluir", views.excluir_avaliacao_template, name='excluir_avaliacao_template'),
     path("<int:id_avaliacao>/excluir/confirmar", views.excluir_avaliacao, name='excluir_avaliacao'),
