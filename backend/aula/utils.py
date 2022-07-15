50dbc4bdb7b066ab9888df7ff7687739529b577d
Adson Matheus <adson.matheus62@gmail.com>
2022-07-15 16:56:37 -0300

    feat: funcao que retorna datas recorrentes #138

--- backend/aula/utils.py
+++ backend/aula/utils.py
@@ -1,3 +1,4 @@
+from datetime import timedelta, datetime
 from aula.models import AulaDoAluno
 from .models import Aluno
 
@@ -7,3 +8,13 @@ def alunos_nao_participantes_de_aula(id_aula):
     """
     alunos_participantes = AulaDoAluno.objects.filter(aula=id_aula).values_list('aluno', flat=True)
     return Aluno.objects.exclude(id__in=alunos_participantes)
+
+
+def datas_recorrentes(data_inicio, data_fim, hora, intervalo):
+    datas = []
+    datas.append(datetime.combine(date = data_inicio, time=hora))
+    while data_inicio <= data_fim:
+        data_inicio += timedelta(days=intervalo)
+        if not data_inicio > data_fim:
+            datas.append(datetime.combine(date = data_inicio, time=hora))
+    return datas
