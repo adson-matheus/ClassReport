8dd127143a29c7b8887ffbfe00294bb7bd30e78f
Adson Matheus <adson.matheus62@gmail.com>
2022-07-15 16:56:19 -0300

    feat: link p/ view que add aulas recorrentes #138

--- backend/aula/urls.py
+++ backend/aula/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("prof/<username>", views.AulaTemplate.index_aula_prof, name='index_aula_prof'),
     path("list", views.AulaTemplate.index_aula_admin, name='index_aula_admin'),
     path("add", views.AulaTemplate.add_aula, name='add_aula'),
+    path("add/recorrentes", views.AulaTemplate.add_aulas_recorrentes, name='add_aulas_recorrentes'),
     path("edit/<int:id>", views.AulaTemplate.edit_aula, name='edit_aula'),
     path("delete/<int:id>", views.AulaTemplate.delete_aula_template, name='delete_aula_template'),
     path("delete/<int:id>/confirm", views.AulaTemplate.delete_aula, name='delete_aula'),
