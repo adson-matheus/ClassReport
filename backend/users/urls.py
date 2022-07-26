343e306ccd7b6bf81e929d4b5dc5d55ec163b691
Adson Matheus <adson.matheus62@gmail.com>
2022-07-26 08:40:17 -0300

    refactor: ajustando view e url com os novos arq. #97

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,16 +1,20 @@
-from users import views
+from users import views, views_aluno, views_professor
 from django.urls import path
 
 app_name = 'users'
 urlpatterns = [
     path('admin/add', views.add_admin_template, name='add_admin_template'),
-    path('alunos', views.AlunoTemplate.index_aluno, name='index_aluno'),
-    path('alunos/add', views.AlunoTemplate.add_aluno, name='add_aluno'),
-    path('alunos/edit/<int:matr>', views.AlunoTemplate.edit_aluno, name='edit_aluno'),
-    path('alunos/delete/<int:matr>', views.AlunoTemplate.delete_aluno_template, name='delete_aluno_template'),
-    path('alunos/delete/<int:matr>/confirm', views.AlunoTemplate.delete_aluno, name='delete_aluno'),
-    path('prof', views.ProfessorTemplate.index_professor, name='index_prof'),
     path('prof/add', views.add_prof_template, name='add_prof_template'),
-    path('prof/delete/<int:siape>', views.ProfessorTemplate.delete_professor_template, name='delete_prof_template'),
-    path('prof/delete/<int:siape>/confirm', views.ProfessorTemplate.delete_professor, name='delete_prof'),
+
+    # alunos
+    path('alunos', views_aluno.index_aluno, name='index_aluno'),
+    path('alunos/add', views_aluno.add_aluno, name='add_aluno'),
+    path('alunos/edit/<int:matr>', views_aluno.edit_aluno, name='edit_aluno'),
+    path('alunos/delete/<int:matr>', views_aluno.delete_aluno_template, name='delete_aluno_template'),
+    path('alunos/delete/<int:matr>/confirm', views_aluno.delete_aluno, name='delete_aluno'),
+
+    # professores
+    path('prof', views_professor.index_professor, name='index_prof'),
+    path('prof/delete/<int:siape>', views_professor.delete_professor_template, name='delete_prof_template'),
+    path('prof/delete/<int:siape>/confirm', views_professor.delete_professor, name='delete_prof'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,21 +1,9 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import permission_required
 from django.contrib import messages
-from users.utils import is_admin, listar_alunos
-from django.contrib.auth.models import User, Group
-from .models import Administrador, Professor, Aluno
-from .forms import AdministradorForm, ProfessorForm, UserForm, AlunoForm, EditarAlunoForm
-from django.http import HttpResponse
-
-def add_admin_controller(form_user, form_admin):
-    '''
-        Cria administrador adicionando o respectivo siape.
-    '''
-    if form_user.is_valid() and form_admin.is_valid():
-        get_siape = form_admin.cleaned_data['siape']
-        user = add_generic_user(form_user=form_user, group_name='grp_administradores')
-        Administrador(user=user, siape=get_siape).save()
-        return True
+from users.utils import is_admin
+from .controller import add_admin_controller, add_prof_controller
+from .forms import AdministradorForm, ProfessorForm, UserForm
 
 @permission_required('users.add_administrador', login_url='/', raise_exception=True)
 def add_admin_template(request):
@@ -39,16 +27,6 @@ def add_admin_template(request):
     context.update(is_admin(request))
     return render(request, 'users/add_admin_template.html', context)
 
-def add_prof_controller(form_user, form_prof):
-    '''
-        Cria professor adicionando o respectivo siape.
-    '''
-    if form_user.is_valid() and form_prof.is_valid():
-        get_siape = form_prof.cleaned_data['siape']
-        user = add_generic_user(form_user=form_user, group_name='grp_professores')
-        Professor(user=user, siape=get_siape).save()
-        return True
-
 @permission_required('users.add_professor', login_url='/', raise_exception=True)
 def add_prof_template(request):
     if request.method == 'POST':
@@ -61,124 +39,7 @@ def add_prof_template(request):
         else:
             messages.error(request, "Erro ao criar professor(a)! Provavelmente já existe um usuário com esse username ou siape.")
     else:
-        form_user = UserForm(request.POST)
-        form_prof = ProfessorForm(request.POST)
+        form_user = UserForm()
+        form_prof = ProfessorForm()
     context = is_admin(request)
     return render(request, 'users/add_prof_template.html', context)
-
-def add_generic_user(form_user, group_name):
-    '''
-        Cria, salva e retorna um User; adiciona User ao seu grupo.
-    '''
-    user = User.objects.create_user(
-        username = form_user.cleaned_data['username'],
-        password = form_user.cleaned_data['password'],
-        email = form_user.cleaned_data['email'],
-        first_name = form_user.cleaned_data['first_name'],
-        last_name = form_user.cleaned_data['last_name'],
-    )
-    get_group = Group.objects.get(name=group_name)
-    get_group.user_set.add(user)
-    return user
-
-class AlunoTemplate:
-    """
-        Aluno possui matrícula e nome. Aluno assiste 'n' aulas.
-        Aluno NÃO possui acesso ao sistema.
-    """
-    @permission_required('users.view_aluno', login_url='/', raise_exception=True)
-    def index_aluno(request):
-        context = listar_alunos()
-        context.update({"full_name": request.user.get_full_name()})
-        context.update(is_admin(request))
-        return render(request, 'users/aluno/alunos.html', context)
-
-    @permission_required('users.add_aluno', login_url='/', raise_exception=True)
-    def add_aluno(request):
-        """
-            Administrador adiciona aluno
-        """
-        if request.method == 'POST':
-            form = AlunoForm(request.POST)
-            if form.is_valid():
-                messages.success(request, "Aluno adicionado com sucesso!")
-                form.save()
-                return redirect('users:index_aluno')
-            else:
-                messages.error(request, "Erro ao cadastrar aluno")
-        else:
-            form = AlunoForm()
-        context = {
-            'form': form,
-            'user': request.user,
-            'full_name': request.user.get_full_name()
-        }
-        context.update(is_admin(request))
-        return render(request, 'users/aluno/add_aluno.html', context)
-
-    @permission_required('users.change_aluno', login_url='/', raise_exception=True)
-    def edit_aluno(request, matr):
-        aluno = get_object_or_404(Aluno, matricula=matr)
-        if request.method == 'POST':
-            form = EditarAlunoForm(request.POST, instance=aluno)
-            if form.is_valid():
-                messages.success(request, "Aluno editado com sucesso!")
-                form.save()
-                return redirect('users:index_aluno')
-            else:
-                messages.error(request, "Erro ao editar aluno")
-        else:
-            form = EditarAlunoForm(instance=aluno)
-        context = {
-            'aluno': aluno,
-            'user': request.user,
-            'full_name': request.user.get_full_name()
-        }
-        context.update(is_admin(request))
-        return render(request, 'users/aluno/edit_aluno.html', context)
-
-    @permission_required('users.delete_aluno', login_url='/', raise_exception=True)
-    def delete_aluno_template(request, matr):
-        aluno = Aluno.objects.get(matricula=matr)
-        context = {
-            'full_name': request.user.get_full_name(),
-            'aluno': aluno,
-        }
-        context.update(is_admin(request))
-        return render(request, 'users/aluno/delete_aluno.html', context)
-
-    @permission_required('users.delete_aluno', login_url='/', raise_exception=True)
-    def delete_aluno(request, matr):
-        aluno = get_object_or_404(Aluno, matricula=matr)
-        aluno.delete()
-        messages.success(request, "Aluno deletado com sucesso!")
-        return redirect('users:index_aluno')
-
-@login_required
-class ProfessorTemplate:
-    """
-        CRUD de Professor
-    """
-    def index_professor(request):
-        context = {
-            'professores': Professor.objects.exclude(user__is_active=False),
-            'full_name': request.user.get_full_name(),
-            'is_admin': True,
-        }
-        return render(request, 'users/prof/professores.html', context)
-
-    def delete_professor_template(request, siape):
-        professor = get_object_or_404(Professor, siape=siape)
-        context = {
-            'full_name': request.user.get_full_name(),
-            'professor': professor,
-        }
-        context.update(is_admin(request))
-        return render(request, 'users/prof/delete_prof.html', context)
-
-    def delete_professor(request, siape):
-        professor = get_object_or_404(Professor, siape=siape)
-        professor.user.is_active = False
-        professor.user.save()
-        messages.success(request, "{} excluído com sucesso!".format(professor))
-        return redirect('users:index_prof')
