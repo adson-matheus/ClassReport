9c73bbae7c57e22f00d3503f51b1b6cfb257c43b
Adson Matheus <adson.matheus62@gmail.com>
2022-08-23 10:54:10 -0300

    feat: static root #147

--- backend/ClassReport/settings.py
+++ backend/ClassReport/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,6 +148,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login/'
 SHORT_DATE_FORMAT = 'd/m/Y %H:%M'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
